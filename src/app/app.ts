4fc07a67d13305811d4beb339507ad3853487bf3
David Weber <david.weber@w3tec.ch>
2016-09-02 22:54:38 +0200

    Example how to use translation in code and make routes translateable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
+import { inject } from 'aurelia-framework';
 import { Router, RouterConfiguration } from 'aurelia-router';
+import { I18N } from 'aurelia-i18n';
 
+@inject(I18N)
 export class App {
   private router: Router;
 
+  constructor(
+    private i18n: I18N
+  ) { }
+
   public configureRouter(config: RouterConfiguration, router: Router): void {
-    config.title = 'Aurelia';
+    config.title = this.i18n.tr('SITE_TITLE');
     config.options.pushState = true;
     config.map([
       {
@@ -12,21 +19,21 @@ export class App {
         name: 'welcome',
         moduleId: './modules/welcome/welcome',
         nav: true,
-        title: 'Welcome'
+        title: this.i18n.tr('WELCOME.TITLE')
       },
       {
         route: 'users',
         name: 'users',
         moduleId: './modules/users/users',
         nav: true,
-        title: 'Github Users'
+        title: this.i18n.tr('GIT_USERS.TITLE')
       },
       {
         route: 'child-router',
         name: 'child-router',
         moduleId: './modules/child-router/child-router',
         nav: true,
-        title: 'Child Router'
+        title: this.i18n.tr('CHILD_ROUTER.TITLE')
       }
     ]);
 
--- src/assets/i18n/en.ts
+++ src/assets/i18n/en.ts
@@ -1,10 +1,17 @@
 const translation = {
 	translation: {
+		'SITE_TITLE': 'Aurelia TS Boilerplate',
 		'WELCOME': {
-			'TITLE': 'Welcome to the Aurelia Navigation App',
+			'TITLE': 'Welcome',
 			'FIRST_NAME': 'First Name',
 			'LAST_NAME': 'Last Name',
 			'FULL_NAME': 'Full Name'
+		},
+		'GIT_USERS': {
+			'TITLE': 'Github Users'
+		},
+		'CHILD_ROUTER': {
+			'TITLE': 'Child Router'
 		}
 	}
 };
