a0b7632890e4a4716cc0eca6c3045789c61d8a63
David Weber <david.weber@w3tec.ch>
2016-11-13 14:28:23 +0100

    update node settings and aurelia linter

--- config/config-aurelia-linter.js
+++ config/config-aurelia-linter.js
@@ -1,33 +1,42 @@
 "use strict";
+const webpack = require('webpack');
 
 /**
  * AureliaTemplateLinter config
  */
 const aureliaTemplateLinter = function (options) {
-	return {
-		module: {
-			preLoaders: [
-				{
-					test: /\.html$/,
-					loader: 'aurelia-template-lint-loader'
-				}
-			]
-		},
-		aureliaTemplateLinter: {
-			configuration: options && options.config,
+  return {
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.html$/,
+          loader: 'aurelia-template-lint-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          aureliaTemplateLinter: {
+            configuration: options && options.config,
 
-			// aurelia errors are displayed by default as warnings
-			// set emitErrors to true to display them as errors
-			emitErrors: false,
+            // aurelia errors are displayed by default as warnings
+            // set emitErrors to true to display them as errors
+            emitErrors: false,
 
-			// aurelia does not interrupt the compilation by default
-			// if you want any file with aurelia errors to fail
-			// set failOnHint to true
-			failOnHint: options.failOnHint,
+            // aurelia does not interrupt the compilation by default
+            // if you want any file with aurelia errors to fail
+            // set failOnHint to true
+            failOnHint: options.failOnHint,
 
-			typeChecking: options.typeChecking,
-			fileGlob: options.sourceDir
-		}
-		};
+            typeChecking: options.typeChecking,
+            fileGlob: options.sourceDir
+          }
+        }
+      })
+    ]
+  };
 };
+
 module.exports = aureliaTemplateLinter;
