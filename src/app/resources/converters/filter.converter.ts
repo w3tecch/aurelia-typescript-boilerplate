ffea5d0b4fd76ed178f0b758beddbc6bdf07bd4f
David Weber <david.weber@w3tec.ch>
2017-03-25 16:20:03 +0100

    Enhance converters

--- src/app/resources/converters/filter.converter.ts
+++ src/app/resources/converters/filter.converter.ts
@@ -1,7 +1,10 @@
 export class FilterValueConverter {
   public toView(array, property, value): any[] {
-    if (!array || !property || !value) {
+    if (!array || !property) {
       return array;
+    }
+    if (!value) {
+      return array.filter((item) => item.indexOf(property) > -1);
     }
     return array.filter((item) => item[property].toLowerCase().indexOf(value.toLowerCase()) > -1);
   }
