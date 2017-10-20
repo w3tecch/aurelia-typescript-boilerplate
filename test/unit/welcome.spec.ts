69dcb40e0f0b326198d3788e641a324818fcc475
David Weber <david.weber@w3tec.ch>
2017-10-20 17:59:43 +0200

    correct some jest tests

--- test/unit/welcome.spec.ts
+++ test/unit/welcome.spec.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from 'aurelia-bootstrapper';
 import { StageComponent } from 'aurelia-testing';
-import { PLATFORM } from 'aurelia-pal';
+import { PLATFORM } from 'aurelia-framework';
 
 describe('WelcomeComponent', () => {
   let component;
